bc5c246cbd5f2fd3e9b9e265abeb50fc0a545710
Siyuan Ren <netheril96@gmail.com>
2026-03-18 09:57:03 +0000

    Ensure clean build

--- matcher-rs/all.sh
+++ matcher-rs/all.sh
@@ -4,14 +4,17 @@ set -e
 # Change directory to the location of this script
 cd "$(dirname "$0")"
 
+WASM_PATH="target/wasm32-unknown-unknown/release/issuance.wasm"
+
 # Run 1.sh to 8.sh and copy the output WASM file after each build
 for i in {1..8}; do
     echo "--- Executing $i.sh ---"
+    rm -rf target
     bash "./$i.sh"
     
     DEST="../app/src/main/assets/openid4vci-nano-$i.wasm"
-    echo "Copying target/wasm32-unknown-unknown/release/issuance.wasm to $DEST"
-    cp target/wasm32-unknown-unknown/release/issuance.wasm "$DEST"
+    echo "Copying $WASM_PATH to $DEST"
+    cp "$WASM_PATH" "$DEST"
 done
 
 echo "All builds completed and files copied."
